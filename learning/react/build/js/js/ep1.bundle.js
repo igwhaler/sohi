ac68008cc44aaf11038c69941abe583e0b5bf473
bigfishguo <691978327@qq.com>
2017-03-20 00:00:06 +0800

    webpacl

---
+++ learning/react/build/js/js/ep1.bundle.js
@@ -0,0 +1,18 @@
+webpackJsonp([0],[/* 0 */
+,/* 1 */
+,/* 2 */
+,/* 3 */
+/***/
+function(e,t,n){"use strict";n(7),n(8);var a=n(1);(0,function(e){return e&&e.__esModule?e:{default:e}}(a).default)("body").css({backgroundColor:"tan"})},/* 4 */
+/***/
+function(e,t,n){"use strict";function a(e){return e&&e.__esModule?e:{default:e}}var l=n(0),u=a(l),i=n(2),o=a(i),d=n(1),r=a(d),s=u.default.createClass({displayName:"ComponentItem",getInitialState:function(){return{like:!0}},componentWillMount:function(){},componentDidMount:function(){},componentWillUpdate:function(e,t){},componentDidUpdate:function(e,t){},render:function(){var e=[this.props,this.state],t=e[0],n=e[1];return u.default.createElement("div",{className:"box1",onClick:this.handleClick},"你好,",t.question," ",u.default.createElement("span",null,"我",n.like?"是":"不是",t.name,"!")," ")},handleClick:function(){var e=[this.props,this.state],t=e[0],n=e[1];this.setState({like:!n.like}),t.handles(t.name)}}),c=["asd","fgh","jkl"],f=u.default.createClass({displayName:"ComponentArr",propTypes:{title:u.default.PropTypes.string.isRequired},getDefaultProps:function(){return{age:24}},getInitialState:function(){return{data:null,loading:!0}},componentDidMount:function(){var e=this;r.default.ajax({url:"//licaishi.sina.com.cn/admin2/api/leftMenu",type:"get",dataType:"jsonp",data:{}}).done(function(t){t&&t.code,e.setState({data:t,loading:!1})}).fail(function(){e.setState({data:null,loading:!1})})},render:function(){var e=this,t=[this.props,this.state],n=t[0],a=t[1];return u.default.createElement("div",{onClick:this.handleClick},c.map(function(t){return u.default.createElement(s,{question:n.question,name:t,handles:e.inputFocus})}),u.default.createElement("input",{type:"text",ref:"myTextInput"}),u.default.createElement(p,{data:a.data,loading:a.loading}))},inputFocus:function(e){this.refs.myTextInput.value=e,this.refs.myTextInput.focus()}}),p=u.default.createClass({displayName:"ComponentTest",render:function(){var e=this.props,t=e.data;return u.default.createElement("div",null,e.loading?u.default.createElement("h2",null,u.default.createElement(m,null)):u.default.createElement("h2",null,t&&0===t.code?u.default.createElement(h,{data:t}):u.default.createElement(g,null)))}}),m=u.default.createClass({displayName:"Loadinng",render:function(){return u.default.createElement("div",null,"loading")}}),h=u.default.createClass({displayName:"LoadSuccess",render:function(){var e=this.props,t=e.data;return u.default.createElement("div",null,t.msg)}}),g=u.default.createClass({displayName:"LoadFail",render:function(){return u.default.createElement("div",null,"加载失败")}});o.default.render(u.default.createElement("div",{className:"container"},u.default.createElement("i",{className:"icon icon-logo"}),u.default.createElement(f,{question:"我是谁？",title:123})),document.body)},/* 5 */
+,/* 6 */
+,/* 7 */
+/***/
+function(e,t){},/* 8 */
+/***/
+function(e,t){},/* 9 */
+,/* 10 */
+,/* 11 */
+/***/
+function(e,t,n){n(3),e.exports=n(4)}],[11]);
---
+++ learning/react/build/js/js/ep2.bundle.js
@@ -0,0 +1,5 @@
+webpackJsonp([1],{/***/
+10:/***/
+function(n,c,t){"use strict";t(5)},/***/
+5:/***/
+function(n,c){}},[10]);
