93068d489177cd0fe6e0fc34f74049cfe91eb1cc
BigFishGuo <691978327@qq.com>
2017-04-06 17:18:01 +0800

    react

---
+++ new/learning/react/src/jsx/components/reactRouterConfig.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import {
+    Router,
+    Route,
+    Link
+} from "react-router"
+
+const App = React.createClass({
+    render() {
+        return <div>
+            <h1>App</h1>
+            <ul></ul>
+        </div>
+    }
+})
