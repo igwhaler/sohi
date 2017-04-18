5c49671f9f95a083f7f37dfad6bd5217531fe21b
BigFishGuo <691978327@qq.com>
2017-04-18 15:51:23 +0800

    vue

---
+++ new/learning/vue/js/components/hello.vue
@@ -0,0 +1,23 @@
+<template>
+    <p>{{greeting}} World!</p>
+</template>
+
+<script>
+    module.exports = {
+        data: {
+            function() {
+                return {
+                    greeting: "Hello"
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    p {
+        font-size: 14px;
+        color: tan;
+        text-align: center;
+    }
+</style>
