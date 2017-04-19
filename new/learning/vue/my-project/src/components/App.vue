38c78973b820e7426ff4e50c2638df7db0b432da
BigFishGuo <691978327@qq.com>
2017-04-19 13:58:36 +0800

    vue

---
+++ new/learning/vue/my-project/src/components/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <img src="../assets/logo.png">
+    <p>123</p>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 14px;
+}
+</style>
---
+++ new/learning/vue/new/src/views/about.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="about">
+        <img src="../images/logo.png">
+        这是{{page}}页面
+    </div>
+</template>
+<style>
+    .about {
+        color: red
+    }
+</style>
+<style lang="stylus">
+    $color = gray;
+    .about
+        color: $color
+</style>
+<script type="text/javascript">
+    require('../css/about.css')
+    require('../css/about.styl')
+    module.exports = {
+        data () {
+            return {
+                page: 'about'
+            }
+        }
+    }
+</script>
