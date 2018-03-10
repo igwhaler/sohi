6dec29b3c17b599e14775a0b50045b8d500eff53
igwhaler <fangshi@xiaomi.com>
2018-03-11 01:10:22 +0800

    ok3

--- learning/parcel/myh5/src/main.js
+++ learning/parcel/myh5/src/main.js
@@ -1,19 +1,2 @@
 import './styles/index.less'
-
-// import 'amfe-flexible'
-import 'lib-flexible'
-// import $ from 'jquery'
-
-/*window.jQuery = $
-require('./js/libs/jquery.fullPage.js')
-
-$(function() {
-  var _fullpage = $('#fullpage')
-
-  _fullpage.show().fullpage({
-    // sectionsColor: [],
-    verticalCentered: false,
-    scrollingSpeed: 600,
-    easing: 'ease-in'
-  });
-});*/
+// import 'lib-flexible'
