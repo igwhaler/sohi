1682cb6a7b5ae7ac4b0d8bdfe3816139ea21d5cb
igwhaler <fangshi@xiaomi.com>
2018-03-12 22:00:06 +0800

    animate

--- learning/parcel/myh5/src/main.js
+++ learning/parcel/myh5/src/main.js
@@ -1,2 +1,35 @@
 import './styles/index.less'
-// import 'lib-flexible'
+import $ from 'webpack-zepto'
+
+$(function () {
+  var $win = $(window);
+  var $wrap = $("#wrapPage");
+  var $p8 = $("#section8");
+
+  var winHeight = $win.height();
+
+  function init () {
+    $wrap.css({display: "block"})
+  };
+  init()
+
+
+  function p8Animate() {
+    var winScrollHeight = $win.scrollTop();
+    var p8Top = $p8.offset().top;
+
+    console.log(winScrollHeight - p8Top)
+
+    if (winScrollHeight - p8Top > -64) {
+      $p8.addClass('p8-animate')
+    }
+  };
+  p8Animate();
+
+
+  $win.on('scroll', function (e) {
+    p8Animate();
+  }, false)
+
+
+})
