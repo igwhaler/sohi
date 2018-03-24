ad8cef84b30991fa73779c31d0227ad17e67422f
igwhaler <fangshi@xiaomi.com>
2018-03-24 12:07:04 +0800

    77

--- learning/parcel/mypc/src/main.js
+++ learning/parcel/mypc/src/main.js
@@ -27,8 +27,6 @@ $(function () {
     var winScrollHeight = $win.scrollTop();
     var p8Top = $p8.offset().top;
 
-    console.log(winScrollHeight - p8Top)
-
     if (winScrollHeight - p8Top > -64) {
       $p8.addClass('p8-animate')
     }
@@ -36,7 +34,51 @@ $(function () {
   p8Animate();
 
 
+
+  function  handlePage() {
+    var _elList = [
+      [$("#section1"), $("#section1").height() + $("#section2").height(), $("#itemP1")],
+      [$("#section2"), $("#section1").height() + $("#section2").height(), $("#itemP1")],
+
+      [$("#section3"), $("#section3").height(), $("#itemP2")],
+
+      [$("#section4"), $("#section4").height() + $("#section5").height() + $("#section6").height(), $("#itemP3")],
+      [$("#section5"), $("#section4").height() + $("#section5").height() + $("#section6").height(), $("#itemP3")],
+      [$("#section6"), $("#section4").height() + $("#section5").height() + $("#section6").height(), $("#itemP3")],
+
+      [$("#section7"), $("#section7").height() + $("#section8").height() + $("#section9").height(), $("#itemP4")],
+      [$("#section8"), $("#section7").height() + $("#section8").height() + $("#section9").height(), $("#itemP4")],
+      [$("#section9"), $("#section7").height() + $("#section8").height() + $("#section9").height(), $("#itemP4")],
+
+      [$("#section10"), $("#section10").height() + $("#section11").height(), $("#itemP5")],
+      [$("#section11"), $("#section10").height() + $("#section11").height(), $("#itemP5")],
+
+      [$("#section12"), $("#section12").height() + $("#section13").height() + $("#section14").height() + $("#section15").height(), $("#itemP6")],
+      [$("#section13"), $("#section12").height() + $("#section13").height() + $("#section14").height() + $("#section15").height(), $("#itemP6")],
+      [$("#section14"), $("#section12").height() + $("#section13").height() + $("#section14").height() + $("#section15").height(), $("#itemP6")],
+      [$("#section15"), $("#section12").height() + $("#section13").height() + $("#section14").height() + $("#section15").height(), $("#itemP6")],
+    ]
+
+    return function () {
+      _elList.forEach(function(item) {
+        var winScrollHeight = $win.scrollTop();
+        var elTop = item[0].offset().top;
+
+        if (winScrollHeight - elTop >= -200 && winScrollHeight - elTop <= item[1]) {
+          if (item[2].hasClass('active')) {
+            return
+          }
+          item[2].addClass('active').siblings().removeClass('active')
+        }
+      })
+    }
+  }
+  var _handelPage  = handlePage()
+
   $win.on('scroll', function (e) {
     p8Animate();
+    _handelPage()
   }, false)
+
+
 })
