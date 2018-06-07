c3a02d8bb7b242b4ccff1a39604ff120d3116447
方势 <fangshi@xiaomi.com>
2018-06-07 16:42:54 +0800

    koa

--- ig/koa/igKoa/index.js
+++ ig/koa/igKoa/index.js
@@ -1,5 +1,9 @@
 const Koa = require('koa')
 const app = new Koa()
+const bodyParser = require('koa-bodyparser')
+const path = require('path')
+const static = require('koa-static')
+const router = require('./router')
 
 /* const loggerSync = require('./middleware/loggerSync')
 app.use(loggerSync()) */
@@ -11,9 +15,17 @@ app.use(async (ctx) => {
   ctx.body = await getRoute(url)
 }) */
 
-const router = require('./router')
+// ctx.body解析中间件
+app.use(bodyParser())
 
+// static静态资源中间件
+app.use(static(
+  path.join(__dirname, './static')
+))
+
+// 路由
 app.use(router.routes()).use(router.allowedMethods())
+
 
 app.listen(9000, () => {
   console.log('port: 9000')
--- ig/koa/igKoa/router/index.js
+++ ig/koa/igKoa/router/index.js
@@ -7,12 +7,39 @@ home.get('/', async (ctx) => {
 
 let page = new Router()
 page.get('/404', async (ctx) => {
+
   ctx.body = '404'
-}).get('/todo', async (ctx) => {
-  ctx.body = 'todo'
-}).get('/ctx', async (ctx) => {
-  ctx.body = ctx
 })
+.get('/todo', async (ctx) => {
+
+  ctx.body = `
+    <form method="POST" action="/page/ctx">
+      <p>userName</p>
+      <input name="userName" /><br/>
+      <p>nickName</p>
+      <input name="nickName" /><br/>
+      <p>email</p>
+      <input name="email" /><br/>
+      <button type="submit">submit</button>
+    </form>
+  `
+})
+.get('/ctx', async (ctx) => {
+  ctx.cookies.set('cid', 'hello world', {
+    domain: '127.0.0.1',
+    path: '/',
+    maxAge: 10 * 60 * 100,
+    expires: new Date('2019-02-15'),
+    httpOnly: false,
+    overwrite: false
+  })
+
+  ctx.body = ctx
+}).post('/ctx', async (ctx) => {
+
+  ctx.body = ctx.request.body
+})
+
 
 let router = new Router()
 router.use('/', home.routes(), home.allowedMethods())
