08c5621a1fc6a97c3f8a72568e4b845350d2aba0
fangshi <fangshi@xiaomi.com>
2018-08-31 16:53:30 +0800

    vuex

--- vue/sohi-vue/src/router/index.js
+++ vue/sohi-vue/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import ViewContainer from '@/view'
 
 Vue.use(Router)
 
@@ -9,7 +10,7 @@ export default new Router({
     {
       path: '/',
       redirect: '/flex',
-      component: () => import('@/view'),
+      component: ViewContainer,
       children: [
         {
           path: 'flex',
