3c54eec4fa5b74904479c4a1b3ceaf9a244a4aea
igwhaler <igwhaler@gmail.com>
2020-10-22 15:39:55 +0800

    🙄

--- react/20200608-new/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('大鱼个锅锅', () => {
-    const { getByText } = render(<App />);
-    const linkElement = getByText(/大鱼个锅锅/i);
-    expect(linkElement).toBeInTheDocument();
-});
