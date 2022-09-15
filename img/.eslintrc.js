e7dc6431f43059cb091a0f9f83f0a6826b72820c
igwhaler <igwhaler@gmail.com>
2022-09-15 15:00:40 +0800

    😆

---
+++ img/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    'extends': [
+        '@ecomfe/eslint-config',
+        '@ecomfe/eslint-config/typescript',
+        '@ecomfe/eslint-config/react',
+    ],
+    'rules': {
+        'no-unreachable-loop': 'off',
+        'no-console': 'off',
+    },
+};
